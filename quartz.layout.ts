76ac93c2404f7a25b1a56babf8744d46e640d97e
resona07 <resona@ccllssd.com>
2024-10-29 20:32:47 +0800

    customize init

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,6 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.RecentNotes(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
